1c7f9956cdf9180a5a66962feb174e7382de2c4e
bpenchev <bpenchev@BGD8H5CZG2>
2019-07-02 16:49:22 +0300

    Section 4 Exercise - Add a form to create/edit a movie.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,20 +9,30 @@ namespace Vidly.Models
 {
     public class Movie
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public Genre Genre { get; set; }
+
         [Required]
+        [Display(Name="Genre")]
         public byte GenreId { get; set; }
+
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         [DataType(DataType.Date)]
         public DateTime DateAdded { get; set; }
+
         [Required]
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
